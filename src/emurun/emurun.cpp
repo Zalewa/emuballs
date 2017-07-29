1f4e1f2ac920084e79d12ccd1def6c013ef1a524
Zalewa <zalewapl@gmail.com>
2017-07-29 14:50:35 +0200

    Add missing licensing info to emurun.cpp.

--- src/emurun/emurun.cpp
+++ src/emurun/emurun.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2017 Zalewa <zalewapl@gmail.com>.
+ *
+ * This file is part of Emuballs Emurun.
+ *
+ * Emuballs Emurun is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Emuballs Emurun is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Emuballs Emurun.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <csignal>
 #include <cstdint>
 #include <codecvt>
