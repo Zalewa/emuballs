7e2c792fd34850eb98ded9332e0270fc133c324c
Zalewa <zalewapl@gmail.com>
2017-08-01 23:31:12 +0200

    emuballs: perf: allow to inline ARM CPU classes.
    * Emuballs::Arm::Cpu
    * Emuballs::Arm::Flags
    * Emuballs::Arm::RegisterSet

--- src/emuballs/armmachine.hpp
+++ src/emuballs/armmachine.hpp
@@ -78,13 +78,23 @@ public:
 		return (*this)[15];
 	}
 
-	void set(int idx, const regval &value);
-	const regval &operator[](int idx) const;
+	void set(int idx, const regval &value)
+	{
+		if (idx == 15)
+			pcChanged = true;
+		regs[idx] = value;
+	}
+
+	const regval &operator[](int idx) const
+	{
+		return regs[idx];
+	}
 
 	inline void resetPcChanged()
 	{
 		pcChanged = false;
 	}
+
 	inline bool wasPcChanged() const
 	{
 		return pcChanged;
@@ -112,24 +122,74 @@ public:
 	constexpr static auto F_ZERO = 1 << Zero;
 	constexpr static auto F_NEGATIVE = 1 << Negative;
 
-	regval dump() const;
-	void store(regval bitset);
-	void set(Bit bit, bool state);
-	bool test(Bit bit) const;
+	regval dump() const
+	{
+		return bits.to_ulong();
+	}
 
-	bool carry() const;
-	bool carry(bool);
+	void store(regval bitset)
+	{
+		bits = std::bitset<32>(bitset);
+	}
 
-	bool overflow() const;
-	bool overflow(bool);
+	void set(Bit bit, bool state)
+	{
+		bits.set(bit, state);
+	}
 
-	bool zero() const;
-	bool zero(bool);
+	bool test(Bit bit) const
+	{
+		return bits.test(bit);
+	}
 
-	bool negative() const;
-	bool negative(bool);
+	bool carry() const
+	{
+		return test(Carry);
+	}
 
-	cpumode cpuMode() const;
+	bool carry(bool flag)
+	{
+		set(Carry, flag);
+		return flag;
+	}
+
+	bool overflow() const
+	{
+		return test(Overflow);
+	}
+
+	bool overflow(bool flag)
+	{
+		set(Overflow, flag);
+		return flag;
+	}
+
+	bool zero() const
+	{
+		return test(Zero);
+	}
+
+	bool zero(bool flag)
+	{
+		set(Zero, flag);
+		return flag;
+	}
+
+	bool negative() const
+	{
+		return test(Negative);
+	}
+
+	bool negative(bool flag)
+	{
+		set(Negative, flag);
+		return flag;
+	}
+
+	cpumode cpuMode() const
+	{
+		return dump() & 0x1f;
+	}
 
 private:
 	std::bitset<32> bits;
@@ -141,26 +201,58 @@ public:
 	/**
 	 * @brief CPSR flags.
 	 */
-	const Flags &flags() const;
-	Flags &flags();
+	const Flags &flags() const
+	{
+		return _flags;
+	}
+
+	Flags &flags()
+	{
+		return _flags;
+	}
 
 	/**
 	 * @brief SPSR flags for current cpumode.
 	 */
-	const Flags &flagsSpsr() const;
-	Flags &flagsSpsr();
+	const Flags &flagsSpsr() const
+	{
+		return flagsSpsr(_flags.cpuMode());
+	}
+
+	Flags &flagsSpsr()
+	{
+		return flagsSpsr(_flags.cpuMode());
+	}
 
 	/**
 	 * @brief SPSR flags for selected cpumode.
 	 */
-	const Flags &flagsSpsr(cpumode mode) const;
-	Flags &flagsSpsr(cpumode mode);
+	const Flags &flagsSpsr(cpumode mode) const
+	{
+		if (mode >= 32)
+			throw std::out_of_range("cpumode must be <= 31, was: " + std::to_string(mode));
+		return _storedFlags[mode];
+	}
 
-	const RegisterSet &regs() const;
-	RegisterSet &regs();
+	Flags &flagsSpsr(cpumode mode)
+	{
+		return const_cast<Flags&>( static_cast<const Cpu&>(*this).flagsSpsr(mode) );
+	}
+
+	const RegisterSet &regs() const
+	{
+		return _regs;
+	}
+
+	RegisterSet &regs()
+	{
+		return _regs;
+	}
 
 private:
-	DPtr<Cpu> d;
+	Flags _flags;
+	Flags _storedFlags[32];
+	RegisterSet _regs;
 };
 
 class Machine
